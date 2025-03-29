63348813400d32ad7a506fc3431e531f90714a0c
GazdagB <gazdagbal@gmail.com>
2025-03-29 15:55:42 +0100

    🦺 Adds validation and aniiamtes error messages in contact

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -1,20 +1,57 @@
 import React from "react";
 import Image from "next/image";
+import { SubmitHandler, useForm } from "react-hook-form";
+import {AnimatePresence, motion } from "motion/react";
+
+type FormFields = {
+  lastName: string;
+  firstName: string;
+  companyName: string;
+  email: string;
+  phoneNumber: string;
+  message: string;
+};
 
 const Contact = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting},
+  } = useForm<FormFields>();
+
+  const onSubmit: SubmitHandler<FormFields> =async (data) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    console.log(data);
+  };
+    
   return (
     <div className="py-20 flex-col lg:flex-row  px-10 flex lg:gap-28 xl:gap-48">
-        <h2 className="text-5xl font-black text-blzs-teal block lg:hidden">KAPCSOLAT</h2>
-      <Image className="hidden lg:block" alt="Kapcsolat" src={"/contact.svg"} width={360} height={360} />
+      <h2 className="text-5xl font-black text-blzs-teal block lg:hidden">
+        KAPCSOLAT
+      </h2>
+      <Image
+        className="hidden lg:block"
+        alt="Kapcsolat"
+        src={"/contact.svg"}
+        width={360}
+        height={360}
+      />
 
       <form
         action="#"
         method="POST"
+        onSubmit={handleSubmit(onSubmit)}
         className="mx-auto mt-16 max-w-xl sm:mt-20"
       >
-        <h2 className="font-bold text-2xl text-blzs-teal">Küldj nekem üzenetet!</h2>
-        <p className="py-5 text-gray-500">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam sunt sed harum numquam temporibus rerum odio odit non quisquam nostrum.</p>
-        <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+        <h2 className="font-bold text-2xl text-blzs-teal">
+          Küldj nekem üzenetet!
+        </h2>
+        <p className="py-5 text-gray-500">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam
+          sunt sed harum numquam temporibus rerum odio odit non quisquam
+          nostrum.
+        </p>
+        <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2 transition-all duration-300">
           <div>
             <label
               htmlFor="last-name"
@@ -24,13 +61,28 @@ const Contact = () => {
             </label>
             <div className="mt-2.5">
               <input
+                {...register("firstName", {
+                  required: "Vezetéknév szükséges!",
+                })}
                 id="first-name"
-                name="first-name"
-                required
+                name="firstName"
                 type="text"
                 autoComplete="given-name"
                 className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-blzs-teal"
               />
+                <AnimatePresence>
+              {errors.firstName && (
+                <motion.div
+                  initial={{ opacity: 0, y: -5, height: 0 }}
+                  animate={{ opacity: 1, y: 0, height: "auto" }}
+                  exit={{ opacity: 0, y: -5, height: 0 }}
+                  transition={{ duration: 0.3 }}
+                  className="text-red-400 overflow-hidden"
+                >
+                  {errors.firstName.message}
+                </motion.div>
+              )}
+              </AnimatePresence>
             </div>
           </div>
           <div>
@@ -42,13 +94,28 @@ const Contact = () => {
             </label>
             <div className="mt-2.5">
               <input
+                {...register("lastName", {
+                  required: "Keresztnév szükséges!",
+                })}
                 id="last-name"
-                name="last-name"
-                required
+                name="lastName"
                 type="text"
                 autoComplete="family-name"
                 className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-blzs-teal"
               />
+               <AnimatePresence>
+              {errors.lastName && (
+                <motion.div
+                  initial={{ opacity: 0, y: -5, height: 0 }}
+                  animate={{ opacity: 1, y: 0, height: "auto" }}
+                  exit={{ opacity: 0, y: -5, height: 0 }}
+                  transition={{ duration: 0.3 }}
+                  className="text-red-400 overflow-hidden"
+                >
+                  {errors.lastName.message}
+                </motion.div>
+              )}
+              </AnimatePresence>
             </div>
           </div>
           <div className="sm:col-span-2">
@@ -60,6 +127,7 @@ const Contact = () => {
             </label>
             <div className="mt-2.5">
               <input
+                {...register("companyName")}
                 id="company"
                 name="company"
                 type="text"
@@ -77,13 +145,32 @@ const Contact = () => {
             </label>
             <div className="mt-2.5">
               <input
+                {...register("email", {
+                  required: "E-mail megadás szükséges",
+                  pattern: {
+                    value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                    message: "Érvénytelen e-mail cím!"
+                  }
+                })}
                 id="email"
                 name="email"
-                type="email"
+                type="text"
                 autoComplete="email"
-                required
                 className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-blzs-teal"
               />
+            <AnimatePresence>
+              {errors.email && (
+                <motion.div
+                  initial={{ opacity: 0, y: -5, height: 0 }}
+                  animate={{ opacity: 1, y: 0, height: "auto" }}
+                  exit={{ opacity: 0, y: -5, height: 0 }}
+                  transition={{ duration: 0.3 }}
+                  className="text-red-400 overflow-hidden"
+                >
+                  {errors.email.message}
+                </motion.div>
+              )}
+              </AnimatePresence>
             </div>
           </div>
           <div className="sm:col-span-2">
@@ -114,22 +201,38 @@ const Contact = () => {
             </label>
             <div className="mt-2.5">
               <textarea
+                {...register("message", {
+                  required: "Üzenet megadása kötelező!",
+                })}
                 id="message"
                 name="message"
                 rows={4}
                 className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-blzs-teal"
-                required
-                defaultValue={""}
               />
+
+            <AnimatePresence>
+              {errors.message && (
+                <motion.div
+                  initial={{ opacity: 0, y: -5, height: 0 }}
+                  animate={{ opacity: 1, y: 0, height: "auto" }}
+                  exit={{ opacity: 0, y: -5, height: 0 }}
+                  transition={{ duration: 0.3 }}
+                  className="text-red-400 overflow-hidden"
+                >
+                  {errors.message.message}
+                </motion.div>
+              )}
+              </AnimatePresence>
             </div>
           </div>
         </div>
         <div className="mt-10">
           <button
+            disabled={isSubmitting}
             type="submit"
             className="block cursor-pointer w-full rounded-md bg-blzs-teal px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2"
           >
-            Beszéljünk!
+            {isSubmitting ? "Küldés...": "Beszéljünk"}
           </button>
         </div>
       </form>
