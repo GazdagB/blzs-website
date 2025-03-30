565ac4d6466ebe3799bb71d6705e5afc4ae5201b
GazdagB <gazdagbal@gmail.com>
2025-03-30 20:20:46 +0200

    🐛 Removed unused var vausing deployement issues

--- src/app/components/SkillBar.tsx
+++ src/app/components/SkillBar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import { delay, motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 interface SkillIcon {
   src: string;
