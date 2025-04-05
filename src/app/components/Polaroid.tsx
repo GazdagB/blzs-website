aeed8b437ddac991f9e17a827bc27d6c3e0a5b33
GazdagB <gazdagbal@gmail.com>
2025-04-05 10:44:26 +0200

    🏷️  Defines PropTypes

--- src/app/components/Polaroid.tsx
+++ src/app/components/Polaroid.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import Image from 'next/image'
 
-const Polaroid = ({src}) => {
+interface PolaroidTypes {
+  src: string
+}
+
+const Polaroid: React.FC<PolaroidTypes> = ({src}) => {
   return (
     <div className='shadow-lg pb-25 scale-75 sm:scale-90 lg:scale-100'>
         <Image width={450} height={450} src={src} alt='Karikatúra rajz'/>
