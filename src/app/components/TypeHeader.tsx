365c14d9d52bc5c592dade539a0f519ba4520b2d
GazdagB <gazdagbal@gmail.com>
2025-04-05 15:12:32 +0200

    📱 Responsive text

--- src/app/components/TypeHeader.tsx
+++ src/app/components/TypeHeader.tsx
@@ -21,14 +21,14 @@ const TypeHeader: React.FC<TypeHeaderProps> = ({
     <div className={`flex flex-col-reverse mt-30 gap-10 ${direction === "left" ? "md:flex-row-reverse" : "md:flex-row"}`}>
       <div
         className={`${
-          direction === "right" ? "text-right" : "text-left"
-        } max-w-[500px] text-pretty flex items-center justify-center`}
+          direction === "right" ? "md:text-right" : "md:text-left"
+        } max-w-[500px] text-pretty flex items-center justify-center px-10 md:px-0 text-center`}
       >
         {bodyText}
       </div>
 
       {/* Headers */}
-      <div className="flex flex-col items-center justify-center text-blzs-teal font-sans relative">
+      <div className="flex flex-col items-center justify-center text-blzs-teal font-sans relative px-10 md:px-0">
         <div className='flex flex-col items-center justify-center relative min-h-[150px] before:content-[""] before:absolute before:bg-white before:h-[70%] before:w-full before:z-10 before:top-0 before:translate-y-[25%] before:left-0'>
           <div className="lowercase text-2xl z-20">{subHeader}</div>
           <div className="uppercase text-6xl font-bold z-20">{header}</div>
