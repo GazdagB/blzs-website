b9d6264a6c594d8965f4af3a407a77f9bbedf56b
GazdagB <gazdagbal@gmail.com>
2025-04-08 18:42:36 +0200

    📱 Responsive layout

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='bg-blzs-teal w-[100vw] text-white flex flex-col justify-between py-5 md:px-20 items-center overflow-hidden'>
-        <div className='flex flex-col items-center overflow-hidden py-2'>
+    <footer className='bg-blzs-teal w-[100vw] text-white flex flex-col md:flex-row justify-between py-5 md:px-20 items-center overflow-hidden'>
+        <div className='flex flex-col items-center md:items-start overflow-hidden py-2'>
             <p><span className='font-bold'>© 2025 Blzs Studio.</span> Minden jog fenntartva.</p>
             <p> Weboldal készítette: <a className='font-bold' href="">Gazdag Studio</a></p>
         </div>
