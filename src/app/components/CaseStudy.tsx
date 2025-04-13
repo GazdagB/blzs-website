d7997b2bc389583630fa0001ce2fb2cdf069087d
GazdagB <gazdagbal@gmail.com>
2025-04-13 16:03:15 +0200

    📱 Makes casetudy components responsive

--- src/app/components/CaseStudy.tsx
+++ src/app/components/CaseStudy.tsx
@@ -6,17 +6,18 @@ import {motion} from "motion/react"
 
 const CaseStudy = () => {
   return (
-    <div className='h-[100vh] w-[100vw] flex'>
-        <div className='h-full w-[50%] bg-gray-200 flex items-center justify-end px-[7%] z-0'>
+    <div className='min-h-[100vh] w-[100vw] flex flex-col lg:flex-row overflow-x-hidden'>
+        <div className='lg:w-[50%] pt-20 bg-white md:bg-gray-200 flex items-end lg:items-center justify-center lg:justify-end lg:px-[7%] z-0'>
             <motion.div
             initial={{ x: 1000, opacity: 0}}
             animate={{ x: 0, opacity: 1}}
             transition={{delay: .5, duration: .5}}
+            className='scale-140 lg:scale-100'
             >
-                <Polaroid width={600} height={600} src={"/references/art/majka.webp"}></Polaroid>
+                <Polaroid bottomPart={false} width={600} height={600} src={"/references/art/majka.webp"}></Polaroid>
             </motion.div>
         </div>
-        <div className='w-[50%] h-full flex items-center justify-start ps-[5%] bg-white z-10'>
+        <div className='min-h-[50vh] lg:w-[50%] py-20 flex items-center justify-center lg:justify-start px-10 lg:px-[5%] bg-white z-10'>
             <motion.div
             initial={{y: 300, opacity: 0}}
             animate={{y: 0, opacity: 1}}
--- src/app/components/Polaroid.tsx
+++ src/app/components/Polaroid.tsx
@@ -5,11 +5,12 @@ interface PolaroidTypes {
   src: string,
   width: number,
   height: number
+  bottomPart? : boolean
 }
 
-const Polaroid: React.FC<PolaroidTypes> = ({src,width, height}) => {
+const Polaroid: React.FC<PolaroidTypes> = ({src,width, height, bottomPart = true}) => {
   return (
-    <div className='shadow-lg pb-25 scale-75 sm:scale-90 lg:scale-100 bg-white'>
+    <div className={`shadow-lg ${bottomPart ? "pb-25" : ""} scale-75 sm:scale-90 lg:scale-100 bg-white`}>
         <Image width={width} height={height} src={src} alt='Karikatúra rajz'/>
     </div>
   )
