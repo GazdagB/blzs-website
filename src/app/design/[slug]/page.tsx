75168d9e7244e8b961c728bb25b8464a1ce62d1a
GazdagB <gazdagbal@gmail.com>
2025-04-15 18:55:17 +0200

    🐛 Removes typscript compiler error

--- src/app/design/[slug]/page.tsx
+++ src/app/design/[slug]/page.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react'
 import { notFound } from 'next/navigation'
 import CaseStudy from '@/app/components/CaseStudy'
@@ -8,19 +7,20 @@ async function getCaseStudy(slug: string) {
     return caseStudies.find((c) => c.slug === slug) || null;
 }
 
-interface CaseStudyParams {
-    params: {
-      slug: string;
-    };
+// Update the interface to use PageProps
+interface PageProps {
+    params: Promise<{
+        slug: string;
+    }>;
 }
 
 export async function generateStaticParams() {
     return caseStudies.map(({ slug }) => ({ slug }));
 }
 
-export default async function CaseStudyPage({ params }: CaseStudyParams) {
+export default async function CaseStudyPage({ params }: PageProps) {
     // Await the params object before accessing its properties
-    const resolvedParams = await Promise.resolve(params);
+    const resolvedParams = await params;
     const caseStudy = await getCaseStudy(resolvedParams.slug);
   
     if (!caseStudy) return notFound();
